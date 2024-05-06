be53794235c4b868c67affcf8182ead3b9f1bf79
BasedBird <basedbird445@gmail.com>
2024-05-06 18:30:06 -0400

    Implemented individual port scan
    individual ports can now be scanned by adding a port param to the URL

--- src/main/java/com/app/PortScanner/PortScannerApplication.java
+++ src/main/java/com/app/PortScanner/PortScannerApplication.java
@@ -4,7 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -21,7 +24,16 @@ public class PortScannerApplication {
 	}
 
 	@GetMapping("/test")
-	public String portScan(HttpServletRequest request) throws InterruptedException {
+	public String portScan(HttpServletRequest request, @RequestParam("port") Optional<String> portParam) throws InterruptedException {
+		String port = portParam.orElse("");
+		if (port.equals("")) return portScanAll(request);
+		PortScannerThread scan = new PortScannerThread(request.getRemoteAddr(), port);
+		scan.start();
+		while(scan.getStatus().equals("UNKNOWN")) Thread.sleep(100);
+		return scan.getStatus();
+	}
+
+	private String portScanAll(HttpServletRequest request) throws InterruptedException {
 		if (PortScanner.IN_PROGRESS) {
 			System.out.println("Rejected");
 			return "Another scan is in progress, please try again later";
